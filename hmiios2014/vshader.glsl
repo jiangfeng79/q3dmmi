a98fe3ced523d1f9c97dade9b32c47b2f6b68c41
Jiang Feng <feng.jiang@rohde-schwarz.com>
2020-03-09 09:45:49 +0800

    further fix for intel glsl issue

--- hmiios2014/vshader.glsl
+++ hmiios2014/vshader.glsl
@@ -1,13 +1,13 @@
 attribute highp vec4 posAttr;
-attribute lowp vec4 colAttr;
-varying lowp vec4 col;
+//attribute lowp vec4 colAttr;
+//varying lowp vec4 col;
 varying lowp vec4 pos;
 uniform lowp vec2 mouse;
 uniform lowp vec2 resolution;
 uniform float time;
 uniform highp mat4 matrix;
 void main() {
-   col = colAttr;
+   //col = colAttr;
    pos = posAttr;
    gl_Position = matrix * posAttr;
 }
